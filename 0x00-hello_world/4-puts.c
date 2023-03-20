217c9a5662bb2bf0a5508d565dfea420e0253b5c
Faith-vaati <vaati.faith79@gmail.com>
2023-03-20 16:25:43 -0700

    scripts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <studio.h>
+/**
+*main - Entry
+* Return: Always 0
+*/
+int main(void)
+{puts(''\" programming is like building a multi lingual puzzle'');
+return (0);
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+/**
+* main - Entry
+* Return: Always 0
+*/
+int main(void)
+{
+printif("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
