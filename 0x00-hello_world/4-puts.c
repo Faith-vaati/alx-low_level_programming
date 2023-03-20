53ce995dd6ff2283e9e076807f4001ecf0c116ec
Faith-vaati <vaati.faith79@gmail.com>
2023-03-20 16:47:57 -0700

    scripts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,5 +4,5 @@
 * Return: Always 0
 */
 int main(void)
-{puts(''\" programming is like building a multi lingual puzzle'');
+{puts(''\" programming is like building a multilingual puzzle'');
 return (0);
