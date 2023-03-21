11961e9ce4ee9a95b5924067950511e67b7ad5ca
Faith-vaati <vaati.faith79@gmail.com>
2023-03-20 17:16:42 -0700

    scripts

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
-#include <studio.h>
+#include <stdio.h>
 /**
-* main - Entry
-* Return: Always 0 (success)
+* main - Entry point
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-printif("Size of a char: %lu byte(s)\n", sizeof(char));
-printif("Size of a int: %lu byte(s)\n", sizeof(int));
-printif("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printif("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printif("Size of a float: %lu byte(s)\n", sizeof(float));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
