2eef5482ffe926d2a080e8bd91900e5c3acbbad1
Faith-vaati <vaati.faith79@gmail.com>
2023-04-25 03:48:33 -0700

    files

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,12 +7,13 @@
 * @str: array str
 *
 * Return: Always 0 (Success)
-*           
+*
 */
 int check_num(char *str)
 {
 		/*Declaring variables*/
 		unsigned int count;
+
 		count = 0;
 		while (count < strlen(str)) /*count string*/
 		{
@@ -28,9 +29,9 @@ int check_num(char *str)
 * main - Print the name of the program
 * @argc: Count arguments
 * @argv: Arguments
-*          
+*
 * Return: Always 0 (Success)
-*         
+*
 **/
 int main(int argc, char *argv[])
 {
@@ -38,6 +39,7 @@ int main(int argc, char *argv[])
 		int count;
 		int str_to_int;
 		int sum = 0;
+
 		count = 1;
 		while (count < argc) /*Goes through the whole array*/
 		{
