67c53fa15ca79e73a5291f73872c6b7f5935ffab
Faith-vaati <vaati.faith79@gmail.com>
2023-04-25 07:26:36 -0700

    changes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -22,25 +22,22 @@ int main(int argc, char *argv[])
 
 	bytes = atoi(argv[1]);
 
-	if (bytes < 0
-			printf(
-				Error\n");
-			exit(2);
-			}
-
-
-		arr = (
-			har *)main;
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+		arr = (char *)main;
 
 		for (i = 0; i < bytes; i++)
 		{
 		if (i == bytes - 1)
 
 		{
-		printf(" % 02hhx\n", arr[i]);
+		printf("%02hhx\n", arr[i]);
 		break;
 		}
-		printf(" % 02hhx ", arr[i]);
+		printf("%02hhx ", arr[i]);
 		}
 return (0);
 }
