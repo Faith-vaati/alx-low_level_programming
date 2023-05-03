20372a2730728aea1d385a308adb394dfb8e9c53
Faith-vaati <vaati.faith79@gmail.com>
2023-05-03 04:17:02 -0700

    files

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,12 +8,7 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int sum1, sum2, y
-
-	sum1 = 0;
-
-	sum2 = 0;
-
+	int sum1 = 0, sum2 = 0, y;
 	for (y = 0; y < size; y++)
 	{
 		sum1 = sum1 + a[y * size + y];
